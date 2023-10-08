2945baa9da1757daff22b2c7a919f8b61e1475b5
PK <nozumaz@gmail.com>
2023-10-08 08:38:32 -0400

    completed functions

--- index.js
+++ index.js
@@ -21,3 +21,57 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(empInput) {
+    const newObj = {
+        firstName : empInput[0],
+        familyName : empInput[1],
+        title : empInput[2],
+        payPerHour : empInput[3],
+        timeInEvents : [],
+        timeOutEvents : []
+    };
+
+    return newObj;
+}
+
+function createEmployeeRecords(empInputs) {
+    return empInputs.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find(e => e.date === date)
+    let timeOut = this.timeOutEvents.find(e => e.date === date)
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+function findEmployeeByFirstName(arr, firstName) {
+    return arr.find(e => e.firstName === firstName)
+}
+
+function calculatePayroll(arr) {
+    return arr.reduce((total, e) => total + allWagesFor.call(e), 0)
+}
